f6647058bc3d77bc013258f779459c398037562b
psato <forhighlow@gmail.com>
2022-01-04 00:29:22 +0900

    feat(test): chrome driver の設定

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,15 @@
-from selenium import webdriver 
+from selenium import webdriver
+#Optionsクラスのインポート（ヘッドレスの設定をするため）
+from selenium.webdriver.chrome.options import Options
 
-browser = webdriver.Firefox(executable_path="./geckodriver")
-browser.get('http://localhost:8000') 
+#Optionsのインスタンスを生成（変数optionsに格納）
+options = Options()
+
+#headlessの設定をTrueにする
+options.headless = True
+
+#webdriverの起動
+browser = webdriver.Chrome(options=options)
+browser.get('http://localhost:8000')
 
 assert 'Django' in browser.title
