9b18db84ed3a1a2d406a533f98994d4ae80cb96d
psato <forhighlow@gmail.com>
2022-05-05 00:59:06 +0900

    First unit test and url mapping, dummy view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,4 +1,12 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        self.assertEquals(found.func, home_page)
 
 
 class SmokeTest(TestCase):
